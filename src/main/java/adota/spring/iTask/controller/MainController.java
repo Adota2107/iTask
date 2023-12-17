a2c6f84b8cc627c2ee2fd33364a31495f8efa67e
Adota2107 <nhatminh21072003@gmail.com>
2023-12-17 16:21:02 +0700

    modified:   pom.xml 	modified:   src/main/java/adota/spring/iTask/controller/MainController.java

--- src/main/java/adota/spring/iTask/controller/MainController.java
+++ src/main/java/adota/spring/iTask/controller/MainController.java
@@ -9,12 +9,13 @@ import adota.spring.iTask.service.TaskService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/iTask")
@@ -24,15 +25,20 @@ public class MainController {
     private TaskService taskService;
 
     @GetMapping("/all")
-    public List<Task> getAllTask() {
-        return taskService.getAllTask();
+    public ResponseEntity<?> getAllTasks(
+        @RequestParam(value = "title", required = false) String title
+    ) {
+        if (ObjectUtils.isEmpty(title)) {
+           return ResponseEntity.ok(taskService.getAllTask());
+        }
+
+        return ResponseEntity.ok(taskService.getTaskByTittle(title));
     }
 
     @GetMapping("/category/{title}")
     public List<Task> getTaskByTitle(@PathVariable String title) {
         return taskService.getTaskByTittle(title);
     }
-
 
     @PostMapping("/add")
     public String addTask(@RequestBody Task task) {
